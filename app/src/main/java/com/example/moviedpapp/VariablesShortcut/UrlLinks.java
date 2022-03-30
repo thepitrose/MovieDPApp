dbf1385793fb8433ab2c6743ac6b81ddba7451a2
ziv <77165570+thepitrose@users.noreply.github.com>
2022-03-30 23:25:03 +0300

    Update UrlLinks.java

--- app/src/main/java/com/example/moviedpapp/VariablesShortcut/UrlLinks.java
+++ app/src/main/java/com/example/moviedpapp/VariablesShortcut/UrlLinks.java
@@ -2,5 +2,5 @@ package com.example.moviedpapp.VariablesShortcut;
 
 public class UrlLinks {
     public static final String BASE_URL = "https://api.themoviedb.org";
-    public static final String API_KEY ="df9d172b9acaeb96759ec844ce7f64bf";
+    public static final String API_KEY ="2c46288716a18fb7aadcc2a801f3fc6b";
 }
